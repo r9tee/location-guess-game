f8d32e99c5836ac1c1d8523630214cdc7e07116e
r9tee <ryanninete@gmail.com>
2025-05-18 10:07:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function showQuestion() {
     if (current < data.length) {
       showQuestion();
     } else {
-      document.body.innerHTML = "<h1>🎉 You finished the game, we hope you enjoyed the trip down memory lane.<br>Happy 50th Birthday Jen! ❤️</h1>";
+      document.body.innerHTML = "<h1>🎉 You finished the game!</h1> <h2>Happy 50th Birthday Jen! ❤️</h2> <p>We love you - Ryan, Makena, Jaxson, Jezebel, and Jilly-Boo</p>";
     }
   };
 }
